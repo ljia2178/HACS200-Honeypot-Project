702192e8f80efc50845260f85da597c14f388f53
Grace Yang <gyang128@terpmail.umd.edu>
2024-10-01 13:27:31 -0400

    Create setup_big_fin.sh

---
+++ setup_big_fin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Catching incorrect params for set up command
+if [[ $# -ne 1 ]];
+then
+	echo "Usage: ./setup_big_fin <Container Name>"
+	exit 1
+fi
+
+container_name=$1
+
+sudo lxc-attach -- bash -c "mkdir Customer_Service Services Activity | cd Customer_Service | mkdir Resources | cd ../Services | mkdir Credit_Card Retirement | cd ../Activity | mkdir Transfers"
+
+sudo lxc file push big_bankacc_customer.csv $container_name/root/Customer_Service/bankacc_customer.csv
+
+sudo lxc file push big_creditcard_info.csv $container_name/root/Services/Credit_Card/creditcard_info.csv 
+
+sudo lxc file push big_moneytransfer_hist.xls $container_name/root/Activity/Transfers/moneytransfer_hist.xls
